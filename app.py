be82f183ac06858a1ce54eb4f2c380f2066ea4cf
AdoringStarling <juanmanueljimenezvergara9@gmail.com>
2022-10-19 16:59:34 -0500

    leyenda fig 2, callback fig 3 y titulo

--- app.py
+++ app.py
@@ -1575,8 +1575,8 @@ def update_figure(n_clicks,TOPO,EXG,START_DATE,END_DATE,MAGN,DEPTH,SEISMO,PPII,C
         
         if np.isin('H2O', PETRO):
             INYO={'display': 'block'}
-            # iny=pd.read_csv(r'datasets\inyeccion_geo.csv',delimiter=';',decimal=',')
-            # iny=iny[:-1]
+            iny=pd.read_csv(r'datasets\inyeccion_geo.csv',delimiter=';',decimal=',')
+            iny=iny[:-1]
             ls_form=[False]*len(iny['CAMPO'])
             ls_form[-1]=True
             for i,_ in zip(iny['CAMPO'],ls_form):
@@ -2159,7 +2159,16 @@ def update_profile(n_clicks,START_DATE,END_DATE,MAGN,DEPTH,SEISMO,x0,x1,y0,y1):
     fig2.update_layout(xaxis_title="Distancia (°)",
                         yaxis_title="Profundidad (m)",
                         font_family="Poppins",
-                        margin=dict(t=50,l=50,b=50,r=50), showlegend = False)
+                        margin=dict(t=50,l=50,b=50,r=50), showlegend = True)
+    fig2.update_layout(legend=dict(
+        orientation="h",
+        #yanchor="bottom",
+        y=-0.2,
+        #xanchor="right",
+        x=0
+    ))
+    fig2.update_layout(autosize = True, height=600)
+    # fig2.update_layout(legend_x=0, legend_y=-1)
     return fig2
 
 @app.callback(
@@ -2169,11 +2178,9 @@ def update_profile(n_clicks,START_DATE,END_DATE,MAGN,DEPTH,SEISMO,x0,x1,y0,y1):
 
 def iny(n_clsick,TINY):
     datos_iny = pd.read_csv("datasets/inyeccion_geo.csv", delimiter = ';')
+    #'Volúmenes de agua (bbl) - '+name_campo
     name_campo=TINY
-    fig = make_subplots(
-        rows=1, cols=1,
-        specs=[[{"type": "scatter"}]],
-        subplot_titles=('Volúmenes de agua (bbl) - '+name_campo))
+    fig = go.Figure()
     months=[]
     for i in datos_iny.columns:
         if '-' in i:
@@ -2183,20 +2190,19 @@ def iny(n_clsick,TINY):
     iny_df=datos_iny[datos_iny['CAMPO']==name_campo]
 
     fig.add_trace(
-        go.Scatter(x=months,y=[float(np.array(iny_df[x])[0]) for x in months],name=name_campo,showlegend=False),
-        row=1, col=1
+        go.Scatter(x=months,y=[float(np.array(iny_df[x])[0]) for x in months],name=name_campo,showlegend=False)
     )
     años=np.arange(2017,2022)
     old=np.array([0,0,0,0,0])
-    fig.update_layout(margin=dict(t=50,l=50,b=50,r=50),hoverlabel=dict(
+    fig.update_layout(margin=dict(t=50,l=50,b=50,r=50)
+                      ,hoverlabel=dict(
                         # bgcolor="white",
                         font_size=12,
-                        font_family="Poppins"
-                    ),barmode='stack',
-                    # title_text="Inyección de agua para recobro mejorado en campos de hidrocarburos (ANH)", 
+                        font_family="Poppins"),
+                    title_text='Volúmenes de agua (bbl) - '+name_campo, 
                     font_family="Poppins")
-    fig.update_yaxes(tickvals=np.arange(0,300000000+1,50000000))
-    fig.update_xaxes(tickvals=años)
+    # fig.update_yaxes(tickvals=np.arange(0,300000000+1,50000000))
+    # fig.update_xaxes(tickvals=años)
     fig.update_xaxes(tickangle=45)
     return fig
 
